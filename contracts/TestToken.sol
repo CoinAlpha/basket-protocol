e6fa3a1134ece71e00c995cd8507f46eb6655d5e
Yvonne Zhang <yzhang1994@Gmail.com>
2018-02-07 11:24:41 -0800

    add comments to test token and node support for deployment

--- contracts/TestToken.sol
+++ contracts/TestToken.sol
@@ -1,24 +1,48 @@
+/*
+
+  Copyright 2018 CoinAlpha, Inc.
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+*/
+
 pragma solidity ^0.4.18;
 import "./lib/StandardToken.sol";
-import "../lib/SafeMath.sol";
+import "./lib/SafeMath.sol";
 
 /**
  * @title TestToken
  * @author CoinAlpha, Inc. <contact@coinalpha.com>
- *
-*/
+ */
 
 contract TestToken is StandardToken, Ownable {
+  string public name;
+  string public symbol;
+  uint public decimals;
+  uint public faucetAmount;
 
-  string name;
-  string symbol;
-  uint decimals;
-  uint faucetAmount;
-
+  /// @dev Token constructor
+  /// @param  _name             Token name
+  /// @param  _symbol           Token symbol
+  /// @param  _decimals         Decimal precision
+  /// @param  _initialSupply    Initial total supply of tokens
+  /// @param  _faucetAmount     Amount to faucet with each request
+  /// @return success           Operation successful
   function TestToken(
     string _name,
     string _symbol,
+    uint _decimals,
     uint _initialSupply,
+    uint _faucetAmount
   )
     public
     returns (bool success)
@@ -26,24 +50,28 @@ contract TestToken is StandardToken, Ownable {
     totalSupply = _initialSupply;
     balances[msg.sender] = _initialSupply;
     owner = msg.sender;
+    name = _name;
+    symbol = _symbol;
+    decimals = _decimals;
+    faucetAmount = _faucetAmount;
   }
 
-  function faucet()
-    public
-    returns (bool)
-  {
+  /// @dev Fallback to reject any ether sent to contract
+  function () public { revert(); }
+
+  /// @dev Withdraw a set amount of token to any address
+  /// @return success           Operation successful
+  function faucet() public returns (bool) {
     balances[owner] = balances[owner].sub(faucetAmount);
     balances[msg.sender] = balances[msg.sender].add(faucetAmount);
     return true;
- }
+  }
 
-  function mint(
-    uint _value
-    returns (bool)
-  )
-    onlyOwner {
-      totalSupply = totalSupply.add(_value);
-      balances[owner] = balances[owner].add(_value);
-      return true;
+  /// @dev Creates new tokens
+  /// @return success           Operation successful
+  function mint(uint _value) onlyOwner returns (bool) {
+    totalSupply = totalSupply.add(_value);
+    balances[owner] = balances[owner].add(_value);
+    return true;
   }
 }
--- truffle.js
+++ truffle.js
@@ -1,29 +1,51 @@
+require('dotenv').config();
+
+const WalletProvider = require("truffle-wallet-provider");
+const Wallet = require('ethereumjs-wallet');
+
+const ropstenPrivateKey = new Buffer(process.env.ROPSTEN_PRIVATE_KEY, "hex")
+const ropstenWallet = Wallet.fromPrivateKey(ropstenPrivateKey);
+const ropstenProvider = new WalletProvider(ropstenWallet, process.env.ROPSTEN_URL);
+
+const kovanPrivateKey = new Buffer(process.env.KOVAN_PRIVATE_KEY, "hex")
+const kovanWallet = Wallet.fromPrivateKey(kovanPrivateKey);
+const kovanProvider = new WalletProvider(kovanWallet, process.env.KOVAN_URL);
+
+const mainNetPrivateKey = new Buffer(process.env.MAINNET_PRIVATE_KEY, "hex")
+const mainNetWallet = Wallet.fromPrivateKey(mainNetPrivateKey);
+const mainNetProvider = new WalletProvider(mainNetWallet, process.env.MAINNET_URL);
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*", // Match any network id
+      network_id: "*",
       gas: 7000000,
     },
     test: {
       host: "localhost",
       port: 8545,
-      network_id: "*", // Match any network id
+      network_id: "*",
       gas: 7000000,
     },
     ropsten: {
-      host: "localhost",
-      port: 8545,
-      network_id: "3",
+      provider: ropstenProvider,
       gas: 7000000,
       gasPrice: 20000000000, // 20 GWei
+      network_id: "3",
+    },
+    kovan: {
+      provider: kovanProvider,
+      gas: 7000000,
+      gasPrice: 20000000000, // 20 GWei
+      network_id: '42',
     },
     mainnet: {
-      host: "localhost",
-      port: 8545,
-      network_id: "1",
+      provider: mainNetProvider,
+      gas: 7000000,
       gasPrice: 20000000000, // 20 GWei
-    }
-  }
+      network_id: "1",
+    },
+  },
 };
