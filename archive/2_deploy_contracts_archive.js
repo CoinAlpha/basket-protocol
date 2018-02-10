a0058f93183d3539268020f1b74219ae8c5959e8
Yvonne Zhang <yzhang1994@Gmail.com>
2018-02-09 19:25:34 -0800

    add utils

---
+++ archive/2_deploy_contracts_archive.js
@@ -0,0 +1,80 @@
+const Basket = artifacts.require('Basket');
+// const BasketFactory6 = artifacts.require('BasketFactory');
+const TestToken = artifacts.require('TestToken');
+const TestTokenFactory = artifacts.require('TestTokenFactory');
+
+// helpers
+const ethToWei = eth => eth * 1e18;
+
+module.exports = (deployer, network, accounts) => {
+  console.log('*************** START CONTRACT DEPLOYMENT ***************');
+
+  // Accounts
+  const ARRANGER = accounts[0];
+  const TOKEN_CREATOR = accounts[0];                 // set token creator to the same address to simplify testing
+
+  let testToken1, testToken2, testToken3;
+
+  if (network === 'development' || network === 'test') {
+    // deployer.deploy(TestTokenFactory, { from: TOKEN_CREATOR })
+    deployer.deploy(
+      TestToken,
+      'test1',                                       // name
+      'TTA',                                         // symbol
+      ethToWei(18),                                  // decimals
+      ethToWei(100),                                 // initialSupply
+      ethToWei(5),                                   // faucetAmount
+      { from: TOKEN_CREATOR },
+    )
+      .then(() => {
+        testToken1 = TestToken;
+        return deployer.deploy(
+          TestToken,
+          'test2',                                     // name
+          'TTB',                                       // symbol
+          ethToWei(18),                                // decimals
+          ethToWei(100),                               // initialSupply
+          ethToWei(5),                                 // faucetAmount
+          { from: TOKEN_CREATOR },
+        );
+      })
+      .then(() => {
+        testToken2 = TestToken;
+        return deployer.deploy(
+          TestToken,
+          'test3',                                     // name
+          'TTC',                                       // symbol
+          ethToWei(10),                                // decimals
+          ethToWei(100),                               // initialSupply
+          ethToWei(8),                                 // faucetAmount
+          { from: TOKEN_CREATOR },
+        );
+      })
+      .then(() => {
+        return deployer.deploy(
+          Basket,
+          'Basket',                                   // name
+          'BSK',                                       // symbol
+          [
+            testToken1.address,
+            testToken2.address,
+            testToken3.address,
+          ],                                           // tokenAddresses
+          [ethToWei(1), ethToWei(2), ethToWei(3)],     // weights
+          { from: ARRANGER },
+        );
+      })
+      .then(() => {
+        console.log('Contracts successfully deployed');
+        return TestToken.deployed();
+      })
+      .then((_testToken) => {
+        console.log(Object.keys(_testToken));
+      })
+      .catch((e) => {
+        console.log('Error: ', e);
+      });
+  } else {
+    console.log('DEPLOYMENT FILE PENDING CONFIG');
+  }
+};
