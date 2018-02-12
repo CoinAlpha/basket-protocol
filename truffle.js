774693ff3c8d9f007727d8b7549ed4aba2e62fba
Carlo P. Las Marias <carlol@gmail.com>
2018-02-12 14:20:27 -0800

    (params) update network gas limits in truffle.js

--- truffle.js
+++ truffle.js
@@ -16,28 +16,40 @@ module.exports = {
     development: {
       host: 'localhost',
       port: 8545,
-      network_id: '*', // Match any network id
-      gas: 7000000,
-      gasPrice: 20000000000, // 20 GWei
+      network_id: "*",       // Match any network id
+      gas: 4700000,          // Set to Ropsten limit
+      gasPrice: 20e9,        // 20 GWei
     },
     test: {
       host: 'localhost',
       port: 8545,
-      network_id: '*', // Match any network id
+      network_id: "*",       // Match any network id
       gas: 7000000,
       gasPrice: 20000000000, // 20 GWei
     },
     ropsten: {
       provider: ropstenProvider,
-      gas: 7000000,
-      gasPrice: 20000000000, // 20 GWei
-      network_id: '3',
+      gas: 4700000,          // Current Ropsten limit is approx 47124331
+      gasPrice: 20e9,        // 20 GWei
+      network_id: "3",
+    },
+    rinkeby: {
+      host: "localhost",
+      gas: 7500000,          // Current approximate limit
+      gasPrice: 20e9,        // 20 GWei
+      network_id: "4",
     },
     kovan: {
       provider: kovanProvider,
-      gas: 7000000,
-      gasPrice: 20000000000, // 20 GWei
+      gas: 6900000,          // Current approximate limit
+      gasPrice: 20e9,        // 20 GWei
       network_id: '42',
+    },
+    mainnet: {
+      provider: mainNetProvider,
+      gas: 8e6,
+      gasPrice: 5e9,         // 5 GWei
+      network_id: "1",
     },
   },
 };
