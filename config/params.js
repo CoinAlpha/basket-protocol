1f6a12da089bf28404f2ed5dac48c0bcd8af724a
Carlo P. Las Marias <carlol@gmail.com>
2018-05-07 11:43:42 -0700

    (refactor) add solidity-coverage config file - truffle network configs for solicity-coverage [IN PROGRESS]

--- config/params.js
+++ config/params.js
@@ -1,6 +1,9 @@
 // Gas price used for development and testing
 const GAS_PRICE_DEV = 20e9;
 
+const GAS_LIMIT = 5e6;
+
 module.exports = {
   GAS_PRICE_DEV,
+  GAS_LIMIT,
 };
--- truffle.js
+++ truffle.js
@@ -1,4 +1,7 @@
-const { GAS_PRICE_DEV } = require('./config/params.js');
+const {
+  GAS_PRICE_DEV,
+  GAS_LIMIT,
+} = require('./config/params.js');
 
 module.exports = {
   networks: {
@@ -18,34 +21,41 @@ module.exports = {
     // },
     ropsten: {
       host: 'localhost',
-      gas: 4700000,          // Current Ropsten limit is approx 4712388
-      gasPrice: 20e9,        // 20 GWei
+      gas: 4700000, // Current Ropsten limit is approx 4712388
+      gasPrice: 20e9, // 20 GWei
       network_id: '3',
     },
     parrotRp: {
       host: '10.1.10.171',
       port: 7545,
-      gas: 4700000,          // Current Ropsten limit is approx 47124331
-      gasPrice: 20e9,        // 20 GWei
+      gas: 4700000, // Current Ropsten limit is approx 47124331
+      gasPrice: 20e9, // 20 GWei
       network_id: '3',
     },
     rinkeby: {
       host: 'localhost',
-      gas: 7500000,          // Current approximate limit
-      gasPrice: 20e9,        // 20 GWei
+      gas: 7500000, // Current approximate limit
+      gasPrice: 20e9, // 20 GWei
       network_id: '4',
     },
     kovan: {
       host: 'localhost',
-      gas: 6900000,          // Current approximate limit
-      gasPrice: 20e9,        // 20 GWei
+      gas: 6900000, // Current approximate limit
+      gasPrice: 20e9, // 20 GWei
       network_id: '42',
     },
     mainnet: {
       host: 'localhost',
       gas: 8e6,
-      gasPrice: 5e9,         // 5 GWei
+      gasPrice: 5e9, // 5 GWei
       network_id: '1',
+    },
+    coverage: {
+      host: 'localhost',
+      port: 8555,
+      gas: GAS_LIMIT,
+      gasPrice: GAS_PRICE_DEV,
+      network_id: '*',
     },
   },
 };
