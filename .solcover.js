9a2174d02e39d0a7d8d832dacabab6e8288ee4e3
Carlo P. Las Marias <carlol@gmail.com>
2018-05-08 16:27:43 -0700

    (refactor) solidity-coverage config

--- .solcover.js
+++ .solcover.js
@@ -1,7 +1,9 @@
+const { GAS_LIMIT } = require('./config/params.js');
+
 module.exports = {
   port: 8555,
-  testrpcOptions: '-p 8555 -l 5000000',
+  testrpcOptions: `-p 8555 -l ${GAS_LIMIT}`,
   norpc: false,
   testCommand: 'truffle test',
-  skipFiles: ['zeppelin/*']
+  skipFiles: ['zeppelin/BasicToken.sol', 'zeppelin/Destructible.sol', 'zeppelin/ERC20.sol', 'zeppelin/ERC20Basic.sol', 'zeppelin/Ownable.sol', 'zeppelin/SafeMath.sol', 'zeppelin/StandardToken.sol', 'TestToken.sol']
 };
