3882184857f382f1059fcc1918c89a9b985a87dc
Yvonne Zhang <yzhang1994@Gmail.com>
2018-05-09 10:44:07 -0700

    (cleanup) delete unused modifier

--- .solcover.js
+++ .solcover.js
@@ -1,4 +1,3 @@
-
 module.exports = {
   norpc: false,
   skipFiles: ['zeppelin/BasicToken.sol', 'zeppelin/Destructible.sol', 'zeppelin/ERC20.sol', 'zeppelin/ERC20Basic.sol', 'zeppelin/Ownable.sol', 'zeppelin/SafeMath.sol', 'zeppelin/StandardToken.sol', 'TestToken.sol']
